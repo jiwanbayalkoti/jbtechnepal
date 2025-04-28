107d0229ffc3c65a729e51f7102c085161eedf06
jiwan bayalkoti <jiwanbayalkoti@gmail.com>
2025-04-28 21:11:04 +0545

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,26 @@ class AppServiceProvider extends ServiceProvider
     {
         // Register components
         Blade::component('admin-form-modal', \App\View\Components\AdminFormModal::class);
+        
+        // Share $footerCategories with the app layout
+        view()->composer('layouts.app', function ($view) {
+            // Get categories for the footer - we'll limit to 5 categories
+            $footerCategories = \App\Models\Category::where('is_featured', 1)
+                                ->orderBy('name')
+                                ->take(5)
+                                ->get();
+            
+            $view->with('footerCategories', $footerCategories);
+        });
+
+        // Share $footerCategories with the app layout
+        view()->composer('layouts.app', function ($view) {
+            // Get categories for the footer - we'll limit to 5 categories
+            $footerCategories = \App\Models\Category::orderBy('name')
+                                ->take(5)
+                                ->get();
+            
+            $view->with('footerCategories', $footerCategories);
+        });
     }
 }
